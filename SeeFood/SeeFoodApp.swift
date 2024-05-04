391591f8aa1a0465e5240da03c1c91e56296e2aa
Amr Magdy <amagdy195@yahoo.com>
2024-05-04 14:29:42 +0300

    Initial Commit

---
+++ SeeFood/SeeFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeFoodApp.swift
+//  SeeFood
+//
+//  Created by Amr Magdy on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SeeFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
